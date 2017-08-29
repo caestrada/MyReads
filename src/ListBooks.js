cb2f41e3701b6f217c455d1209a33d1851b57e54
Carlos Estrada <caestrada@Carloss-MBP.attlocal.net>
2017-08-28 22:40:54 -0400

    refactor: Create BooksGrid component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as api from './BooksAPI';
+import BooksGrid from './BooksGrid';
 
 class ListBooks extends Component {
   state = {
@@ -43,83 +44,32 @@ class ListBooks extends Component {
             <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.state.books.filter( book => book.bookShelf === 'currentlyReading').map( book => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.bookShelf} onChange={(event) => this.changeShelf(event, book.id) }>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
+                <BooksGrid 
+                  shelf="currentlyReading"
+                  books={this.state.books} 
+                  toggleBookShelf={this.changeShelf}
+                />
               </div>
             </div>
 
             <div className="bookshelf">
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.state.books.filter( book => book.bookShelf === 'wantToRead').map( book => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.bookShelf} onChange={(event) => this.changeShelf(event, book.id) }>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
+                <BooksGrid 
+                  shelf="wantToRead"
+                  books={this.state.books}
+                  toggleBookShelf={this.changeShelf}
+                />
               </div>
             </div>
             <div className="bookshelf">
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.state.books.filter( book => book.bookShelf === 'read').map( book => (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select value={book.bookShelf} onChange={(event) => this.changeShelf(event, book.id) }>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
+                <BooksGrid 
+                  shelf="read"
+                  books={this.state.books}
+                  toggleBookShelf={this.changeShelf}
+                />
               </div>
             </div>
           </div>
